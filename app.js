621bb3fd0b3bf5352657f1f7afe48792e04bb2b8
Erik Huang <ekhu94@gmail.com>
2021-04-24 20:41:41 -0700

    creates conditional rendering to index page

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>New Message</title></head>");
+    res.write(
+      '<body><form method="POST" action="/message"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.end("Welcome to my node server!");
 });
 
 server.listen(3000, "localhost");
